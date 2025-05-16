847cba361a4cb467a26b24cabb91ad88ea917d61
Trevor Waters <twater623@west-mec.org>
2025-05-16 13:58:59 -0700

    changing file extentions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-import { LoadingScreen } from "./components/LoadingScreen";
-import { Navbar } from "./components/Navbar";
-import { MobileMenu } from "./components/MobileMenu";
-import { Home } from "./components/sections/Home";
-import { About } from "./components/sections/About";
-import { Projects } from "./components/sections/Projects";
+import { LoadingScreen } from "./components/LoadingScreen.jsx";
+import { Navbar } from "./components/Navbar.jsx";
+import { MobileMenu } from "./components/MobileMenu.jsx";
+import { Home } from "./components/sections/Home.jsx";
+import { About } from "./components/sections/About.jsx";
+import { Projects } from "./components/sections/Projects.jsx";
 import "./index.css";
-import { Contact } from "./components/sections/Contact";
+import { Contact } from "./components/sections/Contact.jsx";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
